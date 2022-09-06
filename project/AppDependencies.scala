411035db182bad8466801224d59f2990984abb1e
hmrc-web-operations <hmrc-web-operations@digital.hmrc.gov.uk>
2022-09-06 09:28:42 +0000

    'Creating new service income-tax-additional-information'

---
+++ project/AppDependencies.scala
@@ -0,0 +1,17 @@
+import play.core.PlayVersion
+import play.sbt.PlayImport._
+import sbt.Keys.libraryDependencies
+import sbt._
+
+object AppDependencies {
+
+  val compile = Seq(
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "7.2.0",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.71.0"
+  )
+
+  val test = Seq(
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "7.2.0"             % "test, it",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % "0.71.0"            % Test,
+  )
+}
