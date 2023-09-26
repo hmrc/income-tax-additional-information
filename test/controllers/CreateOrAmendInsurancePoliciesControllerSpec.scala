bbdaf4f33abad759cf653b4093da58ad9de40b56
Juamal Blackman <juamal.blackman@digital.hmrc.gov.uk>
2023-09-26 11:51:17 +0100

    update model tests

--- test/controllers/CreateOrAmendInsurancePoliciesControllerSpec.scala
+++ test/controllers/CreateOrAmendInsurancePoliciesControllerSpec.scala
@@ -47,7 +47,7 @@ class CreateOrAmendInsurancePoliciesControllerSpec extends TestSuite {
   val mtditid = "1234567890"
 
   val model: CreateOrAmendInsurancePoliciesModel = CreateOrAmendInsurancePoliciesModel(
-    lifeInsurance = Seq(LifeInsuranceModel(Some("RefNo13254687"), Some("Life"), 123.45, Some(true), Some(4), Some(3), Some(123.45))),
+    lifeInsurance = Some(Seq(LifeInsuranceModel(Some("RefNo13254687"), Some("Life"), 123.45, Some(true), Some(4), Some(3), Some(123.45)))),
     capitalRedemption = Some(Seq(CapitalRedemptionModel(Some("RefNo13254687"), Some("Capital"), 123.45, Some(true), Some(3), Some(2), Some(0)))),
     lifeAnnuity = Some(Seq(LifeAnnuityModel(Some("RefNo13254687"), Some("Life"), 0, Some(true), Some(2), Some(22), Some(123.45)))),
     voidedIsa = Some(Seq(VoidedIsaModel(Some("RefNo13254687"), Some("isa"), 123.45, Some(123.45), Some(5), Some(6)))),
