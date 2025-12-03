dd23c25558394a9887b2093f9cfe2827dc0f06c0
nadinJ <63252254+nadinJ@users.noreply.github.com>
2025-12-03 16:17:44 +0000

    MIPR-2271: Dep updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,8 +3,8 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapBackendPlay30Version = "10.2.0"
-  private val hmrcMongoPlayVersion = "2.10.0"
+  private val bootstrapBackendPlay30Version = "10.4.0"
+  private val hmrcMongoPlayVersion = "2.11.0"
 
   private val jacksonAndPlayExclusions: Seq[InclusionRule] = Seq(
     ExclusionRule(organization = "com.fasterxml.jackson.core"),
@@ -17,14 +17,14 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                   %% "bootstrap-backend-play-30"  % bootstrapBackendPlay30Version,
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-30"         % hmrcMongoPlayVersion,
-    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.20.0",
+    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.20.1",
     "com.beachape"                  %% "enumeratum"                 % "1.9.0",
     "com.beachape"                  %% "enumeratum-play-json"       % "1.9.0" excludeAll (jacksonAndPlayExclusions *)
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapBackendPlay30Version,
-    "org.scalamock"           %% "scalamock"                  % "7.5.0"
+    "org.scalamock"           %% "scalamock"                  % "7.5.2"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
